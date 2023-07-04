a60ce4b1b4a393e67b9e51aeb2dc11ce51ad2d82
hiro0214 <leonhurt2@gmail.com>
2023-07-04 19:45:33 +0900

    env: add linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
+  root: "src",
   plugins: [react()],
-})
+  server: {
+    port: 8080,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@/": `${__dirname}/src/`,
+    },
+  },
+});
