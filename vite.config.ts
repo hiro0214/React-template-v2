49313e618ce921a476251b6484084575c506fe3c
hiro0214 <leonhurt2@gmail.com>
2023-07-14 11:22:56 +0900

    fix: add host config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  root: "src",
+  base: '/',
+  root: 'src',
   plugins: [react()],
   server: {
     port: 8080,
     open: true,
+    host: true
   },
   resolve: {
     alias: {
-      "@/": `${__dirname}/src/`,
-    },
-  },
+      '@/': `${__dirname}/src/`
+    }
+  }
 });
